9292a9323a0f2bd483bcedda21cb5c543a476065
zhouztashinqz <zhouztashin.qz@gmail.com>
2019-10-11 11:28:00 +0800

    [重构代码]优化Kotlin实现，添加注释。

--- src/main/java/com/snowofsunflower/android/image/EnjoyImages.kt
+++ src/main/java/com/snowofsunflower/android/image/EnjoyImages.kt
@@ -5,44 +5,61 @@ import com.snowofsunflower.image_component.R
 
 /**
  * Created by zhouztashin on 2018/10/31.
+ * 简易的图片使用方式
  */
 
-class EnjoyImages {
+object EnjoyImages {
 
-    companion object {
-        //多个参数的情况，有没有好的实现方式
-        private var imageRound: ImageRound? = null
-        private var imageCircle: ImageCircle? = null
-        fun pureLoader(context: Context, res: Int) = GlideImageLoader(context).load(res)
-        fun pureLoader(context: Context, res: String) = GlideImageLoader(context).load(res)
-        fun loader(context: Context, res: Int) = pureLoader(context, res).placeholder(R.drawable.com_image_load_placeholder).error(R.drawable.com_image_load_error)
-        fun loader(context: Context, res: String) = pureLoader(context, res).placeholder(R.drawable.com_image_load_placeholder).error(R.drawable.com_image_load_error)
-        fun roundLoader(context: Context, res: Int): ImageLoaderConfig {
-            if (imageRound == null) {
-                imageRound = ImageRound(context.resources.getDimension(R.dimen.round))
+    private var imageRound: ImageRound? = null
+    private var imageCircle: ImageCircle? = null
+    /**
+     * 封装成Any，便于之后扩展
+     */
+    //TODO 用什么样的实现方式才可以比较好的把？去掉
+    fun pureLoader(context: Context, res: Any): ImageLoaderConfig? {
+        var loader: ImageLoaderConfig? = null
+        return res?.let {
+            when (it) {
+                it is Int -> {
+                    val value = res as Int
+                    loader = GlideImageLoader(context).load(value)
+                }
+                it is String -> {
+                    val value = res as String
+                    loader = GlideImageLoader(context).load(value)
+                }
             }
-            return loader(context, res).shape(imageRound!!)
+            loader
         }
+    }
 
-        fun roundLoader(context: Context, res: String): ImageLoaderConfig {
-            if (imageRound == null) {
-                imageRound = ImageRound(context.resources.getDimension(R.dimen.round))
-            }
-            return loader(context, res).shape(imageRound!!)
-        }
+    /**
+     * 常用图片加载器
+     */
+    fun loader(context: Context, res: Any): ImageLoaderConfig? {
+        return pureLoader(context, res)?.placeholder(R.drawable.com_image_load_placeholder)?.error(R.drawable.com_image_load_error)
+    }
 
-        fun circleLoader(context: Context, res: Int): ImageLoaderConfig {
-            if (imageCircle == null) {
-                imageCircle = ImageCircle()
-            }
-            return loader(context, res).shape(imageCircle!!)
-        }
+    /**
+     * 圆角图片加载器
+     */
+    fun roundLoader(context: Context, res: Any): ImageLoaderConfig? {
 
-        fun circleLoader(context: Context, res: String): ImageLoaderConfig {
-            if (imageCircle == null) {
-                imageCircle = ImageCircle()
-            }
-            return loader(context, res).shape(imageCircle!!)
+        if (imageRound == null) {
+            imageRound = ImageRound(context.resources.getDimension(R.dimen.round))
         }
+        val shape = imageRound as IImageShape
+        return loader(context, res)?.shape(shape)
+    }
+
+    /**
+     * 圆形图片加载器
+     */
+    fun circleLoader(context: Context, res: String): ImageLoaderConfig? {
+        if (imageCircle == null) {
+            imageCircle = ImageCircle()
+        }
+        val shape = imageCircle as IImageShape
+        return loader(context, res)?.shape(shape)
     }
 }
